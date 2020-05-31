6f01f2f01d9b33a4ae9e479aada0cde966c126a0
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2020-05-31 16:35:06 +0100

    Fix lint

---
+++ src/renderer/app.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import Application from './components/Application';
+
+const mainElement = document.createElement('div');
+document.body.appendChild(mainElement);
+
+const render = (Component: () => JSX.Element) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        mainElement
+    );
+};
+
+render(Application);
---
+++ src/renderer/components/Application.tsx
@@ -0,0 +1,6 @@
+import { hot } from 'react-hot-loader/root';
+import * as React from 'react';
+
+const Application = () => <div>Hello World from Electron!</div>;
+
+export default hot(Application);
